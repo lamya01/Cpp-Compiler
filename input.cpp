ee6db6fbc033ebd9227355eafd72830c69d94095
lamya01 <45792513+lamya01@users.noreply.github.com>
2020-04-02 20:37:28 +0530

    Update and rename testing.cpp to input.cpp

---
+++ input.cpp
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+
+void main()
+{
+	
+	int a=10,b=5,c,d;
+	float j;
+    
+	if(a>b)
+	{
+		c=a+b; 
+	}
+	else
+	{ 
+		c=a-b;
+	}	
+
+	d = a * b ;
+
+	for(int k=0;k<3;k++){
+		c=a-b;
+		
+	}
+	a = a + b;
+}
+
